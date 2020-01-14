9cc466e177f0654352a6d0150fe5184145092126
Joshua Rodriguez <ewized@gmail.com>
2020-01-14 07:54:48 -0800

    Split css into scss modules

--- BlueMapCore/src/main/webroot/js/site.js
+++ BlueMapCore/src/main/webroot/js/site.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import BlueMap from './libs/BlueMap.js';
 
-import '../style/style.css';
+import '../style/style.scss';
 
 $(document).ready(() => {
 	window.blueMap = new BlueMap($('#map-container')[0], 'data/');
