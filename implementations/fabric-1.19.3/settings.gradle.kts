8aae8de8284c4d657d8eec59e83eafda4f02b4b9
Lukas Rieger (Blue) <TBlueF@users.noreply.github.com>
2022-11-24 21:17:39 +0100

    Add Fabric 1.19.3 implementation

---
+++ implementations/fabric-1.19.3/settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "fabric-1.19.3"
+
+includeBuild("../../BlueMapCommon")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ includeBuild("implementations/fabric-1.16.2")
 includeBuild("implementations/fabric-1.17")
 includeBuild("implementations/fabric-1.18")
 includeBuild("implementations/fabric-1.19")
+includeBuild("implementations/fabric-1.19.3")
 
 includeBuild("implementations/forge-1.14.4")
 includeBuild("implementations/forge-1.15.2")
