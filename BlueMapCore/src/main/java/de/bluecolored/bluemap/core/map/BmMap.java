12b75f64a1045fe1fe0d11b0f4e6d945791e3dd9
Lukas Rieger (Blue) <TBlueF@users.noreply.github.com>
2023-02-01 21:42:43 +0100

    Dont load the full meta-file only to test if it exists

--- BlueMapCore/src/main/java/de/bluecolored/bluemap/core/map/BmMap.java
+++ BlueMapCore/src/main/java/de/bluecolored/bluemap/core/map/BmMap.java
@@ -138,7 +138,7 @@ public class BmMap {
 
         // only save texture gallery if not present in storage
         try {
-            if (storage.readMeta(id, META_FILE_TEXTURES).isEmpty())
+            if (storage.readMetaInfo(id, META_FILE_TEXTURES).isEmpty())
                 saveTextureGallery();
         } catch (IOException e) {
             Logger.global.logError("Failed to read texture gallery", e);
