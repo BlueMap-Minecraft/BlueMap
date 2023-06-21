e66838cdbcc07857fafbe9d2cb6d739f9fc4c533
Lukas Rieger (Blue) <TBlueF@users.noreply.github.com>
2023-06-21 15:11:48 +0200

    Add supressed exception for more info

--- BlueMapCore/src/main/java/de/bluecolored/bluemap/core/util/FileHelper.java
+++ BlueMapCore/src/main/java/de/bluecolored/bluemap/core/util/FileHelper.java
@@ -61,7 +61,11 @@ public class FileHelper {
         } catch (IOException ex) {
             try {
                 Files.move(from, to, StandardCopyOption.REPLACE_EXISTING);
-            } catch (FileNotFoundException | NoSuchFileException ignore) {}
+            } catch (FileNotFoundException | NoSuchFileException ignore) {
+            } catch (IOException ex2) {
+                ex2.addSuppressed(ex);
+                throw ex2;
+            }
         }
     }
 
