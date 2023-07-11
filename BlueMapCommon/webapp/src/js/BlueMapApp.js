074bc4a4367e4b5a40d8ae2248c2d1b05ce0bc42
Lukas Rieger (Blue) <TBlueF@users.noreply.github.com>
2023-07-11 20:07:10 +0200

    Fix not able to teleport to players on other worlds. Fixes: #451

--- BlueMapCommon/webapp/src/js/BlueMapApp.js
+++ BlueMapCommon/webapp/src/js/BlueMapApp.js
@@ -349,7 +349,7 @@ export class BlueMapApp {
         return new Promise((resolve, reject) => {
             let loader = new FileLoader();
             loader.setResponseType("json");
-            loader.load(map.data.dataUrl + "live/players?" + generateCacheHash(),
+            loader.load(map.data.dataUrl + "live/players.json?" + generateCacheHash(),
                 fileData => {
                     if (!fileData) reject(`Failed to parse '${this.fileUrl}'!`);
                     else resolve(fileData);
