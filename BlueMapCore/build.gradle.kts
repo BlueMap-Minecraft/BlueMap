78904b405143a8b6851d73bc6577d9f37b97c4ab
Lukas Rieger (Blue) <TBlueF@users.noreply.github.com>
2023-12-06 19:24:07 +0100

    Add resource-patch to make 1.20.3 resources more backwardscompatible with 1.20.2

--- BlueMapCore/build.gradle.kts
+++ BlueMapCore/build.gradle.kts
@@ -113,7 +113,7 @@ tasks.processResources {
 
 //resource Extensions
 val resourceIds: Array<String> = arrayOf(
-    "1_13", "1_15", "1_16", "1_18"
+    "1_13", "1_15", "1_16", "1_18", "1_20_3"
 )
 
 tasks.register("zipResourceExtensions") {
