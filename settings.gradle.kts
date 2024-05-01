499cafd743af8f21bcccae49a9385b1ff81bb1e1
Lukas Rieger (Blue) <github@bluecolored.de>
2024-05-01 11:58:52 +0200

    Add fabric 1.20.5 build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,23 +5,24 @@ val releaseNotesFile = file("release.md")
 if (!releaseNotesFile.exists()) releaseNotesFile.createNewFile();
 
 // implementations
-includeBuild("implementations/cli")
+//includeBuild("implementations/cli")
 
-includeBuild("implementations/fabric-1.15.2")
-includeBuild("implementations/fabric-1.16.2")
-includeBuild("implementations/fabric-1.17")
-includeBuild("implementations/fabric-1.18")
-includeBuild("implementations/fabric-1.19.4")
-includeBuild("implementations/fabric-1.20")
+//includeBuild("implementations/fabric-1.15.2")
+//includeBuild("implementations/fabric-1.16.2")
+//includeBuild("implementations/fabric-1.17")
+//includeBuild("implementations/fabric-1.18")
+//includeBuild("implementations/fabric-1.19.4")
+//includeBuild("implementations/fabric-1.20")
+includeBuild("implementations/fabric-1.20.5")
 
-includeBuild("implementations/forge-1.17.1")
-includeBuild("implementations/forge-1.18.1")
-includeBuild("implementations/forge-1.19.4")
-includeBuild("implementations/forge-1.20")
+//includeBuild("implementations/forge-1.17.1")
+//includeBuild("implementations/forge-1.18.1")
+//includeBuild("implementations/forge-1.19.4")
+//includeBuild("implementations/forge-1.20")
 
-includeBuild("implementations/neoforge-1.20.2")
+//includeBuild("implementations/neoforge-1.20.2")
 
-includeBuild("implementations/spigot")
-includeBuild("implementations/paper")
+//includeBuild("implementations/spigot")
+//includeBuild("implementations/paper")
 
-includeBuild("implementations/sponge")
+//includeBuild("implementations/sponge")
