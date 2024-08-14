18f3eab3c916305997dbf7a6907e192201b01af3
zhangyuheng <zhangyuheng@lunadeer.cn>
2024-08-14 11:30:50 +0800

    add logs the detailed error message in php

--- BlueMapCommon/webapp/public/sql.php
+++ BlueMapCommon/webapp/public/sql.php
@@ -149,7 +149,10 @@ if (startsWith($path, "/maps/")) {
     try {
         $sql = new PDO("$driver:host=$hostname;port=$port;dbname=$database", $username, $password);
         $sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e ) { error(500, "Failed to connect to database"); }
+    } catch (PDOException $e ) { 
+        error_log($e->getMessage(), 0); // Logs the detailed error message
+        error(500, "Failed to connect to database"); 
+    }
 
     // provide map-tiles
     if (startsWith($mapPath, "tiles/")) {
