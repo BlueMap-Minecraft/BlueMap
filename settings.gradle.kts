44e8da7388992e4601ef21bfdf98e2cc8b7fcc18
Lukas Rieger (Blue) <github@bluecolored.de>
2025-03-30 13:15:30 +0200

    Reenable cli build to fix docker workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ includeBuild("api")
 include(":core")
 include(":common")
 
-//implementation("cli")
+implementation("cli")
 //implementation("fabric")
 implementation("forge")
 //implementation("neoforge")
