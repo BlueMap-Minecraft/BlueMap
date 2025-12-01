fb79d394decdffa1a0a50a4ea3d6cece62a1ed13
Lukas Rieger <TBlueF@users.noreply.github.com>
2025-12-02 00:04:56 +0100

    Allow remapping blockstates and block properties via addons (#759)
    * Add abillity for resourcepack-extensions to define blockstate-properties and alter the blockstate id to resource mapping
    
    * Add caching for key-mapping

--- core/src/main/java/de/bluecolored/bluemap/core/resources/pack/resourcepack/ResourcePack.java
+++ core/src/main/java/de/bluecolored/bluemap/core/resources/pack/resourcepack/ResourcePack.java
@@ -87,6 +87,8 @@ public class ResourcePack extends Pack {
 
     @Getter private final BlockColorCalculatorFactory colorCalculatorFactory;
     private final BlockPropertiesConfig blockPropertiesConfig;
+
+    private final LoadingCache<de.bluecolored.bluemap.core.world.BlockState, BlockState> blockStateCache;
     private final LoadingCache<de.bluecolored.bluemap.core.world.BlockState, BlockProperties> blockPropertiesCache;
 
     private final Map<Extension<?>, ResourcePackExtension> extensions;
@@ -104,6 +106,11 @@ public class ResourcePack extends Pack {
         this.colorCalculatorFactory = new BlockColorCalculatorFactory();
         this.blockPropertiesConfig = new BlockPropertiesConfig();
 
+        this.blockStateCache = Caffeine.newBuilder()
+                .executor(BlueMap.THREAD_POOL)
+                .maximumSize(10000)
+                .expireAfterAccess(1, TimeUnit.MINUTES)
+                .build(this::loadBlockState);
         this.blockPropertiesCache = Caffeine.newBuilder()
                 .executor(BlueMap.THREAD_POOL)
                 .maximumSize(10000)
@@ -355,15 +362,36 @@ public class ResourcePack extends Pack {
         return new Atlas();
     }
 
+    public BlockState getBlockState(de.bluecolored.bluemap.core.world.BlockState blockState) {
+        return blockStateCache.get(blockState);
+    }
+
+    private BlockState loadBlockState(de.bluecolored.bluemap.core.world.BlockState blockState) {
+        Key key = blockState.getId();
+        for (ResourcePackExtension extension : extensions.values()) {
+            key = extension.getBlockStateKey(key);
+        }
+        return blockStates.get(key);
+    }
+
     public BlockProperties getBlockProperties(de.bluecolored.bluemap.core.world.BlockState state) {
         return blockPropertiesCache.get(state);
     }
 
     private BlockProperties loadBlockProperties(de.bluecolored.bluemap.core.world.BlockState state) {
-        BlockProperties.Builder props = blockPropertiesConfig.getBlockProperties(state).toBuilder();
+        BlockProperties.Builder props = BlockProperties.builder();
 
+        // collect properties from extensions
+        for (ResourcePackExtension extension : extensions.values()) {
+            extension.getBlockProperties(state, props);
+        }
+
+        // explicitly configured properties always have priority -> overwrite
+        props.from(blockPropertiesConfig.getBlockProperties(state));
+
+        // calculate culling and occlusion from model if UNDEFINED
         if (props.isOccluding() == Tristate.UNDEFINED || props.isCulling() == Tristate.UNDEFINED) {
-            BlockState resource = blockStates.get(state.getId());
+            BlockState resource = getBlockState(state);
             if (resource != null) {
                 resource.forEach(state,0, 0, 0, variant -> {
                     Model model = variant.getModel().getResource(models::get);
