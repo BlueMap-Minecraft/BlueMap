2d48369e63d9c730d7fc1010d1362051d38c24c2
Lukas Rieger <github@bluecolored.de>
2025-12-11 11:24:06 +0100

    Workaround a paper-issue

--- implementations/paper/src/main/java/de/bluecolored/bluemap/bukkit/BukkitCommandSource.java
+++ implementations/paper/src/main/java/de/bluecolored/bluemap/bukkit/BukkitCommandSource.java
@@ -59,17 +59,25 @@ public class BukkitCommandSource implements CommandSource {
     public Optional<Vector3d> getPosition() {
         if (delegate.getSender() instanceof ConsoleCommandSender) return Optional.empty();
 
-        Location location = delegate.getLocation();
-        return Optional.of(new Vector3d(location.getX(), location.getY(), location.getZ()));
+        try {
+            Location location = delegate.getLocation();
+            return Optional.of(new Vector3d(location.getX(), location.getY(), location.getZ()));
+        } catch (NullPointerException ex) { // workaround for a paper-bug: https://github.com/PaperMC/Paper/issues/13387
+            return Optional.empty();
+        }
     }
 
     @Override
     public Optional<ServerWorld> getWorld() {
         if (delegate.getSender() instanceof ConsoleCommandSender) return Optional.empty();
 
-        World world = delegate.getLocation().getWorld();
-        if (world == null) return Optional.empty();
-        return Optional.of(BukkitPlugin.getInstance().getServerWorld(world));
+        try {
+            World world = delegate.getLocation().getWorld();
+            if (world == null) return Optional.empty();
+            return Optional.of(BukkitPlugin.getInstance().getServerWorld(world));
+        } catch (NullPointerException ex) { // workaround for a paper-bug: https://github.com/PaperMC/Paper/issues/13387
+            return Optional.empty();
+        }
     }
 
 }
